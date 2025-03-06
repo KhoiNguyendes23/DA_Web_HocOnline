42ec2a1c0e81af68bf947868a1bb0dc0ce0cd3c2
Tan Dung <tandung29022004@gmail.com>
2025-03-06 14:19:15 +0700

    Khởi tạo project

---
+++ KTDAH/Do_An_Web_Hoc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Do_An_Web_Hoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
