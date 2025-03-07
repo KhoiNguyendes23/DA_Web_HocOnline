c296749e18b4f5450048c91247b39938284f1ef5
KhoiNguyendes23 <khoind82@gmail.com>
2025-03-07 15:51:48 +0700

    Xong giao diện Contact

---
+++ KTDAH/Do_An_Web_Hoc/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Do_An_Web_Hoc.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
