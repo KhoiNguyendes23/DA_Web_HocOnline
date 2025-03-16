e6b62f5b80d529f7ce21d1e60318b7e0400e3e51
Tan Dung <tandung29022004@gmail.com>
2025-03-16 15:57:50 +0700

    update

---
+++ Do_An_Web_Hoc/Repositories/EFCatogoriesRepository.cs
@@ -0,0 +1,11 @@
+﻿using Do_An_Web_Hoc.Models;
+
+namespace Do_An_Web_Hoc.Repositories
+{
+    public class EFCatogoriesRepository : ICatogoriesRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+
+    }
+}
---
+++ Do_An_Web_Hoc/Repositories/Interfaces/ICatogoriesRepository.cs
@@ -0,0 +1,24 @@
+﻿using Do_An_Web_Hoc.Models;
+
+namespace Do_An_Web_Hoc.Repositories.Interfaces
+{
+    public interface ICatogoriesRepository
+    {
+        // lấy tất cả danh mục khóa học
+        Task<IEnumerable<Categories>> GetAllCategoriesAsync();
+        // lấy danh mục theo ID
+        Task<Categories> GetCategoryByIdAsync(int categoryId);
+        // thêm danh mục mới
+        Task AddCategoryAsync(Categories category);
+        // cập nhật thông tin danh mục
+        Task<bool> UpdateCategoryAsync(Categories category);
+        // xóa danh mục
+        Task DeleteCategoryAsync(int categoryId);
+        // Tìm kiếm danh mục theo tên
+        Task<IEnumerable<Categories>> SearchCategoriesByNameAsync(string keyword);
+        // xóa mềm danh mục
+        Task<bool> SoftDeleteCategoryAsync(int categoryId);
+
+
+    }
+}
