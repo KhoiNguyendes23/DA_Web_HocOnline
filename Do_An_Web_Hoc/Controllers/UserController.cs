2a925db2b8dfb16802a5250ec3604265f33b7fa0
TuanAnh0207 <tuananhhh0207@gmail.com>
2025-03-28 15:51:40 +0700

    task3

--- Do_An_Web_Hoc/Controllers/UserController.cs
+++ Do_An_Web_Hoc/Controllers/UserController.cs
@@ -8,7 +8,81 @@ namespace Do_An_Web_Hoc.Controllers
     {
         public IActionResult Dashboard()
         {
-            Console.WriteLine("[DEBUG] Đã vào Dashboard");
+            return View();
+        }
+
+        public IActionResult Courses()
+        {
+            return View();
+        }
+        
+        public IActionResult Completed()
+        {
+            return View();
+        }
+
+        public IActionResult RegisteredCourses()
+        {
+            return View();
+        }
+
+        public IActionResult InProgress()
+        {
+            return View();
+        }
+
+        public IActionResult FreeCourses()
+        {
+            return View();
+        }
+
+        public IActionResult PaidCourses()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz()
+        {
+            return View();
+        }
+
+        public IActionResult Practice()
+        {
+            return View();
+        }
+
+        public IActionResult Ranking()
+        {
+            return View();
+        }
+
+        public IActionResult Support()
+        {
+            return View();
+        }
+
+        public IActionResult ContactLecturer()
+        {
+            return View();
+        }
+
+        public IActionResult Community()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult PaymentHistory()
+        {
+            return View();
+        }
+
+        public IActionResult SecuritySettings()
+        {
             return View();
         }
     }
