9cd70f4cd8fdaa9d32fba6fa3a96141f99b2a7f4
handzvcl <truonghan4002@gmail.com>
2025-04-01 15:37:28 +0700

    Han Done Task 6

--- Do_An_Web_Hoc/wwwroot/js/dropdown-menu.js
+++ Do_An_Web_Hoc/wwwroot/js/dropdown-menu.js
@@ -33,6 +33,33 @@
         }
     });
 
+    // Xử lý menu cấp 2 cho mobile
+    navMenu.addEventListener("click", (event) => {
+        if (event.target.closest(".has-submenu > a") && window.innerWidth <= mediaSize) {
+            event.preventDefault();
+            const parentItem = event.target.closest(".has-submenu");
+
+            // Toggle menu cấp 2
+            const submenu = parentItem.querySelector(".sub-dropdown-menu");
+
+            if (parentItem.classList.contains("open-submenu")) {
+                submenu.style.maxHeight = null;
+                parentItem.classList.remove("open-submenu");
+            } else {
+                // Ẩn tất cả menu cấp 2 khác
+                navMenu.querySelectorAll(".has-submenu").forEach(item => {
+                    item.classList.remove("open-submenu");
+                    const sub = item.querySelector(".sub-dropdown-menu");
+                    if (sub) sub.style.maxHeight = null;
+                });
+
+                submenu.style.maxHeight = submenu.scrollHeight + "px";
+                parentItem.classList.add("open-submenu");
+            }
+        }
+    });
+
+
     function collapseDropdownMenu() {
         navMenu.querySelector(".dropdown-menu-branch.active .custom-dropdown-menu").removeAttribute("style");
         navMenu.querySelector(".dropdown-menu-branch.active").classList.remove("active");
